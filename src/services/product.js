87ebaff3ff0d543514906f1521866e51413ee3a1
xaositectt <xaositectt@gmail.com>
2019-09-30 01:42:11 +0200

    added product object with images

---
+++ src/services/product.js
@@ -0,0 +1,23 @@
+import headsetBlack from '../assets/black.jpg'
+import headsetBrown from '../assets/brown.jpg'
+
+export default {
+  name: 'Audio Technica ATH-MSR7',
+  subTitle: '2017 Best Headphones of the Year Award Winner',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+  details: 'Ham followed now ecstatic use speaking exercise may repeated. Himself he evident oh greatly my on inhabit general concern. It earnest amongst he showing females so improve in picture. Mrs can hundred its greater account. Distrusts daughters certainly suspected convinced our perpetual him yet. Words did noise taken right state are since. Style too own civil out along. Perfectly offending attempted add arranging age gentleman concluded. Get who uncommonly our expression ten increasing considered occasional travelling. Ever read tell year give may men call its. Piqued son turned fat income played end wicket. To do noisy downs round an happy books.',
+  actualPrice: 59.99,
+  oldPrice: 89.99,
+  variations: [
+    {
+      color: 'black',
+      img: headsetBlack,
+      default: true
+    },
+    {
+      color: 'brown',
+      img: headsetBrown,
+      default: false
+    }
+  ]
+}
