759e9fb6aad663348e069c3b9408e90cd19ebf68
xaositectt <xaositectt@gmail.com>
2019-09-30 03:02:58 +0200

    added responsive container in product view, adjusted responsivity of elements

--- src/components/content_box.js
+++ src/components/content_box.js
@@ -8,7 +8,7 @@ ContentBox.propTypes = {
 
 export default function ContentBox({children, innerClass}) {
   return (
-    <section className={'dark-gray pa4 pl5 ' + innerClass}>
+    <section className={'dark-gray pa2 ph3 pa3-ns ph4-ns pa4-xl pl5-xl ' + innerClass}>
       {children}
     </section>
   )
--- src/views/product.js
+++ src/views/product.js
@@ -3,6 +3,8 @@ import ContentBox from '../components/content_box'
 import ProductButton from '../components/product/product_button'
 import ProductTabs from '../components/product/product_tabs'
 import { Button } from 'antd'
+import style from '../styles/modules/product_item.module.scss'
+import imageBlack from '../assets/black.jpg'
 
 import product from '../services/product'
 export default function ProductItem() {
@@ -20,20 +22,28 @@ export default function ProductItem() {
 
   return (
     <div>
-      <ContentBox>
-        <ProductButton onClick={handleBackButtonClick} label={'ALL PRODUCTS'} icon={'arrow-left'}/>
-        <br />
-        <div className={'f4 f3-ns f2-l fw7 mt4-m mt5-xl'}>{product.name}</div>
-        <div className={'light-gray2 f6 f5-ns fw5'}>{product.subTitle}</div>
-      </ContentBox>
+      <div className={'flex flex-column-reverse flex-row-m vh-100-ns'}>
+        <section className={'flex flex-column w-50-m p0 bn br-m b--light-gray'}>
+          <ContentBox>
+            <ProductButton onClick={handleBackButtonClick} label={'ALL PRODUCTS'} icon={'arrow-left'}/>
+            <br />
+            <div className={'f4 f3-ns f2-l fw7 mt4-m mt5-xl'}>{product.name}</div>
+            <div className={'light-gray2 f6 f5-ns fw5'}>{product.subTitle}</div>
+          </ContentBox>
 
-      <ProductTabs product={product} handleDropdownClick={handleDropdownClick} />
+          <ProductTabs product={product} handleDropdownClick={handleDropdownClick} />
 
-      <ContentBox innerClass={'bt b--light-gray mt-auto'}>
-        <Button type="primary" className={'mv4 '} onClick={clickAddToCart} style={{height: '50px'}}>
-          <span className={'fw8'}>ADD TO CART</span>
-        </Button>
-      </ContentBox>
+          <ContentBox innerClass={'bt b--light-gray mt-auto'}>
+            <Button type="primary" className={'mv3 mv4-ns ' + style.product_button} onClick={clickAddToCart} style={{height: '50px'}}>
+              <span className={'fw8'}>ADD TO CART</span>
+            </Button>
+          </ContentBox>
+        </section>
+
+        <section className={'vh-100-m w-50-m flex-m items-center justify-center'}>
+          <img src={imageBlack} className={'db ' + style.product_image}/>
+        </section>
+      </div>
     </div>
   )
 }
